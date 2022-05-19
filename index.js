bc82761cd4bed1dd0ed670da4d0f1591bc59c4b2
DaoNik <nikitataranin@gmail.com>
2022-05-19 23:02:49 +0300

    feed: подключил бота к бд

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const {
   newProblemsOptions,
 } = require('./options');
 
+const sequelize = require('./db');
+const UserModel = require('./models');
+const User = require('./models');
+
 const token = process.env.TOKEN;
 
 const bot = new TelegramApi(token, {
@@ -17,6 +21,7 @@ const bot = new TelegramApi(token, {
 const chats = {};
 
 async function startBot(chatId) {
+  await UserModel.create({ chatId });
   await bot.sendMessage(
     chatId,
     `Приветствую тебя в телеграм боте для управления задачами.
@@ -29,9 +34,10 @@ async function startBot(chatId) {
 }
 
 async function sendInfo(chatId, msg) {
+  const user = await UserModel.findOne({ chatId });
   await bot.sendMessage(
     chatId,
-    `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`
+    `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}, в игре у тебя правильных ответов ${user.right}, неправильных ${user.wrong}`
   );
 }
 
@@ -45,7 +51,14 @@ async function startGame(chatId) {
   await bot.sendMessage(chatId, 'Отгадывай', gameOptions);
 }
 
-const start = () => {
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+  } catch (e) {
+    console.log('Подключение к бд нарушено', e);
+  }
+
   bot.setMyCommands([
     { command: '/start', description: 'Начальное приветствие' },
     { command: '/info', description: 'Информация о пользователе' },
@@ -61,28 +74,32 @@ const start = () => {
     const text = msg.text;
     const typeChat = msg.chat.type;
 
-    if (text.startsWith('/start')) {
-      return startBot(chatId);
-    }
-    if (text.startsWith('/info')) {
-      return sendInfo(chatId, msg);
-    }
-    if (text.startsWith('/game')) {
-      return startGame(chatId);
-    }
-    if (text.startsWith('/problem') && typeChat === 'private') {
-      return bot.sendMessage(chatId, 'Выберите действие', taskOptions);
-    }
-    if (text.startsWith('/problem')) {
-      return bot.sendMessage(
-        chatId,
-        'Перейдите в чат с ботом, чтобы сообщить о проблеме',
-        newProblemsOptions
-      );
+    try {
+      if (text.startsWith('/start')) {
+        return startBot(chatId);
+      }
+      if (text.startsWith('/info')) {
+        return sendInfo(chatId, msg);
+      }
+      if (text.startsWith('/game')) {
+        return startGame(chatId);
+      }
+      if (text.startsWith('/problem') && typeChat === 'private') {
+        return bot.sendMessage(chatId, 'Выберите действие', taskOptions);
+      }
+      if (text.startsWith('/problem')) {
+        return bot.sendMessage(
+          chatId,
+          'Перейдите в чат с ботом, чтобы сообщить о проблеме',
+          newProblemsOptions
+        );
+      }
+    } catch (e) {
+      return bot.sendMessage(chatId, 'К сожалению, произошла ошибка');
     }
   });
 
-  bot.on('callback_query', (msg) => {
+  bot.on('callback_query', async (msg) => {
     const data = msg.data;
     const chatId = msg.message.chat.id;
 
@@ -90,25 +107,27 @@ const start = () => {
     if (data === '/again') {
       return startGame(chatId);
     }
-    // if (data === '/newProblem') {
-    //   return bot.sendMessage(chatId, 'Введите название проблемы');
-    // }
+
     if (data === '/updateProblem') {
       return bot.sendMessage(chatId, 'Пока что нечего менять...');
     }
+    const user = await UserModel.findOne({ chatId });
     if (data === chats[chatId].toString()) {
-      return bot.sendMessage(
+      user.right += 1;
+      await bot.sendMessage(
         chatId,
         `Поздравляю, ты отгадал цифру ${chats[chatId]}`,
         againOptions
       );
     } else {
-      return bot.sendMessage(
+      user.wrong += 1;
+      await bot.sendMessage(
         chatId,
         `К сожалению, ты не угадал, бот загадал ${chats[chatId]}`,
         againOptions
       );
     }
+    await user.save();
   });
 };
 
